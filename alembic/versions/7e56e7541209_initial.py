4f40523384d254a29e24bb18275ef39f47a08873
Chris <chris@niduroki.net>
2021-06-22 14:14:24 +0200

    Make initial migration work if the tables exist already

--- alembic/versions/7e56e7541209_initial.py
+++ alembic/versions/7e56e7541209_initial.py
@@ -24,43 +24,46 @@ def downgrade():
 
 
 def upgrade():
-    op.create_table('users',
-    sa.Column('id', sa.INTEGER(), nullable=False),
-    sa.Column('username', sa.VARCHAR(), nullable=True),
-    sa.Column('password', sa.VARCHAR(), nullable=True),
-    sa.Column('admin', sa.BOOLEAN(), nullable=True),
-    sa.CheckConstraint('admin IN (0, 1)'),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('username')
-    )
-    op.create_table('config',
-    sa.Column('id', sa.INTEGER(), nullable=False),
-    sa.Column('key', sa.VARCHAR(), nullable=True),
-    sa.Column('value', sa.VARCHAR(), nullable=True),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('key')
-    )
-    op.create_table('sites',
-    sa.Column('id', sa.INTEGER(), nullable=False),
-    sa.Column('owner_id', sa.INTEGER(), nullable=True),
-    sa.Column('name', sa.VARCHAR(), nullable=True),
-    sa.Column('seo_description', sa.VARCHAR(), nullable=True),
-    sa.Column('seo_author', sa.VARCHAR(), nullable=True),
-    sa.Column('image', sa.VARCHAR(), nullable=True),
-    sa.Column('bio', sa.VARCHAR(), nullable=True),
-    sa.Column('footer', sa.VARCHAR(), nullable=True),
-    sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('image'),
-    sa.UniqueConstraint('name')
-    )
-    op.create_table('Links',
-    sa.Column('id', sa.INTEGER(), nullable=False),
-    sa.Column('site_id', sa.INTEGER(), nullable=True),
-    sa.Column('icon', sa.VARCHAR(), nullable=True),
-    sa.Column('link', sa.VARCHAR(), nullable=True),
-    sa.Column('text', sa.VARCHAR(), nullable=True),
-    sa.Column('order', sa.INTEGER(), nullable=True),
-    sa.ForeignKeyConstraint(['site_id'], ['sites.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
+    try:
+        op.create_table('users',
+        sa.Column('id', sa.INTEGER(), nullable=False),
+        sa.Column('username', sa.VARCHAR(), nullable=True),
+        sa.Column('password', sa.VARCHAR(), nullable=True),
+        sa.Column('admin', sa.BOOLEAN(), nullable=True),
+        sa.CheckConstraint('admin IN (0, 1)'),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('username')
+        )
+        op.create_table('config',
+        sa.Column('id', sa.INTEGER(), nullable=False),
+        sa.Column('key', sa.VARCHAR(), nullable=True),
+        sa.Column('value', sa.VARCHAR(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('key')
+        )
+        op.create_table('sites',
+        sa.Column('id', sa.INTEGER(), nullable=False),
+        sa.Column('owner_id', sa.INTEGER(), nullable=True),
+        sa.Column('name', sa.VARCHAR(), nullable=True),
+        sa.Column('seo_description', sa.VARCHAR(), nullable=True),
+        sa.Column('seo_author', sa.VARCHAR(), nullable=True),
+        sa.Column('image', sa.VARCHAR(), nullable=True),
+        sa.Column('bio', sa.VARCHAR(), nullable=True),
+        sa.Column('footer', sa.VARCHAR(), nullable=True),
+        sa.ForeignKeyConstraint(['owner_id'], ['users.id'], ),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('image'),
+        sa.UniqueConstraint('name')
+        )
+        op.create_table('Links',
+        sa.Column('id', sa.INTEGER(), nullable=False),
+        sa.Column('site_id', sa.INTEGER(), nullable=True),
+        sa.Column('icon', sa.VARCHAR(), nullable=True),
+        sa.Column('link', sa.VARCHAR(), nullable=True),
+        sa.Column('text', sa.VARCHAR(), nullable=True),
+        sa.Column('order', sa.INTEGER(), nullable=True),
+        sa.ForeignKeyConstraint(['site_id'], ['sites.id'], ),
+        sa.PrimaryKeyConstraint('id')
+        )
+    except sa.exc.OperationalError:
+        pass  # Tables probably exist already - skip
